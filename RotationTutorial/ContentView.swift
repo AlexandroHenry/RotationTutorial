20d4ede44f6fee5a96b103ee54bdc64ae76082d9
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-02-11 18:35:58 +0900

    3D Rotation Basic

--- RotationTutorial/ContentView.swift
+++ RotationTutorial/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State private var degrees = 0.0
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		VStack {
+			Spacer()
+			
+			LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+				.frame(width: 300, height: 200)
+				.cornerRadius(10)
+				.rotation3DEffect(.degrees(degrees), axis: (x: 1, y: 1, z: 1))
+			
+			Spacer()
+			
+			Slider(value: $degrees, in: 0...1080)
+				.padding()
+			
+			Button("Animate") {
+				withAnimation {
+					self.degrees += 180
+				}
+			}
+		}
     }
 }
 
