7bc44fc71be11f90f0291283819eb6297ef11e36
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-02-11 18:30:10 +0900

    Initial Commit

---
+++ RotationTutorial/RotationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationTutorialApp.swift
+//  RotationTutorial
+//
+//  Created by Seungchul Ha on 2023/02/11.
+//
+
+import SwiftUI
+
+@main
+struct RotationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
